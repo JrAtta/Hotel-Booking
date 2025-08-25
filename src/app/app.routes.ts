5e30fb307a9ba13f01c9c074c38a45461f1befcf
heka <Aee23530@gmail.com>
2025-08-25 10:57:48 +0300

    finish register UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // Register
+  {
+    path: 'register',
+    title: 'Register',
+    loadComponent: () =>
+      import('./components/register/register').then((m) => m.Register)
+  },
+
+  // Login
+  {
+    path: 'login',
+    title: 'Login',
+    loadComponent: () =>
+      import('./components/login/login').then((m) => m.Login)
+  },
+
+
+];
---
+++ src/app/components/register/register.ts
@@ -0,0 +1,13 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [NgStyle, RouterLink],
+  templateUrl: './register.html',
+  styleUrl: './register.scss'
+})
+export class Register {
+
+}
