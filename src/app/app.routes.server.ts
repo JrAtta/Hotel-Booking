d6eac9ae8a598615afab2b07894fcc4c888b6730
heka <Aee23530@gmail.com>
2026-02-07 09:14:57 +0200

    fix prerender error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,14 +3,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'booking-page/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'search/hotels/:destination/:personsCount/:startDate/:endDate',
+    renderMode: RenderMode.Server,
   },
   {
     path: 'activity-details/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
+  },
+  {
+   path: 'hotel-details/:id',
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
